2a092f14938cb22ac81ffd14d34fc2abe375fa89
Markysha <12345678+mbrauska-bit@users.noreply.github.com>
2026-05-26 13:17:14 +0200

    Update Simon game

--- js/main.js
+++ js/main.js
@@ -17,99 +17,91 @@ if (startButton !== null) {
 }
 
 // Code for game.html
-//Game page buttons
+// Game page buttons
 const redButton = document.querySelector("#red");
 const blueButton = document.querySelector("#blue");
 const greenButton = document.querySelector("#green");
 const yellowButton = document.querySelector("#yellow");
 
 if (redButton !== null) {
-    //Elements
     const gameStart = document.querySelector("#gameStart");
     const restart = document.querySelector("#restart");
     const scoreText = document.querySelector("#score");
 
-    //A list with four colors. Array
     const colorButtons = [redButton, blueButton, greenButton, yellowButton];
 
-    //Varriables
     let pattern = [];
-    let playerTurn = 0;
+    let playerPlace = 0;
     let score = 0;
     let canClick = false;
 
-    // Choose a random color
-    function getRandomColor()//Creates a function that chooses a random color
-     {
-        const randomNumber = Math.floor(Math.random() * 4);//Makes a random number 0,1,2,3. We have 4 color buttons
-        return colorButtons[randomNumber];//reurns one button from our array
+    function flash(button) {
+        button.classList.add("active");
+
+        setTimeout(function() {
+            button.classList.remove("active");
+        }, 500);
     }
 
-    function wait(ms) {
-        return new Promise(function(done) {
-            setTimeout(done, ms);
-        });
+    function addRandomButton() {
+        const randomNumber = Math.floor(Math.random() * 4);
+        const randomButton = colorButtons[randomNumber];
+        pattern.push(randomButton);
     }
 
-  async function flash(button){
-    button.classList.add("active");
-    await wait(500); //Waits 0.5 sek
-    button.classList.remove("active")
-  }
+    function showPattern() {
+        let i = 0;
+        canClick = false;
 
-    window.panelClicked = function(clickedTile) {
-        if (canClick === true) {
-            const rightTile = pattern[playerTurn]; 
+        const timer = setInterval(function() {
+            flash(pattern[i]);
+            i = i + 1;
 
-         //Player clicked the correct color
-            if (clickedTile === rightTile)//Only lets the player click if canClick is true.
-                 {
-                playerTurn = playerTurn + 1;//Moves the player to the next step in the pattern.
-                //So if the player just clicked step 0, now they are on the step 1.
-
-                if (playerTurn === pattern.length)//Checks if the player has finished the whole pattern correctly
-                     {
-                    score = score + 1;// Add one points to the score
-                    scoreText.textContent = "Score: " + score;// Updates the text on the page
-                    canClick = false;// Stops the player from clicking while the next round is being prepared
-                    setTimeout(nextRound, 1000);
-                }
-                // Player clicked the wrong color
+            if (i === pattern.length) {
+                clearInterval(timer);
+                canClick = true;
             }
-            
-            else {
-                canClick = false;// Stops the player from clicking anymore after losing
-                alert("Game over");// Shows a popup saying Game over!
-            }
-        } 
-    };
+        }, 900);
+    }
 
-    async function nextRound() // async means the function can use await inside it
-    {
-        canClick = false;// Stops the player from clicking anymore after losing
-        playerTurn = 0;// Resets the player postion back to the start of the pattern
-        //For example, if the pattern has 4 colors, the player must start by clicking the first one again
-        pattern.push(getRandomColor());
-
-        for (let i = 0; i < pattern.length; i = i + 1)// Starts a loop that goes trought eevry color in the pattern
-             {
-            const button = pattern[i];
-            await flash(button);// flashes the button
-            await wait(600); // This waits 600 milliseknds=0.6 sek
-        }
-
-        canClick = true;// Allow the player to click after the pattern is shown
+    function nextRound() {
+        playerPlace = 0;
+        addRandomButton();
+        showPattern();
     }
 
     function startGame() {
         pattern = [];
-        playerTurn = 0;// Resets the player back to the first click
-        score = 0;// Resets the score to 0
-        canClick = false;// The player cannot click yet beacause the computer needs to show the pattern first
-        scoreText.textContent = "Score: 0";// Changes the text of the score
+        playerPlace = 0;
+        score = 0;
+        scoreText.textContent = "Score: 0";
         nextRound();
     }
-    //Without these lines , the Start and the Restart buttons will not fo anything
+
+    window.panelClicked = function(clickedTile) {
+        if (canClick === false) {
+            return;
+        }
+
+        if (clickedTile !== pattern[playerPlace]) {
+            canClick = false;
+            alert("Game over");
+            return;
+        }
+
+        playerPlace = playerPlace + 1;
+
+        if (playerPlace === pattern.length) {
+            score = score + 1;
+            scoreText.textContent = "Score: " + score;
+            canClick = false;
+
+            setTimeout(function() {
+                nextRound();
+            }, 1000);
+        }
+    };
+
     gameStart.addEventListener("click", startGame);
     restart.addEventListener("click", startGame);
 }
